fbe7dd1864c5b4a5acf53bfa120ce6babe8728bd
GiorgiChkhitunidze <112623714+GiorgiChkhitunidze@users.noreply.github.com>
2023-05-30 16:34:53 +0400

    update

--- titanic-app.py
+++ titanic-app.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import numpy as np
 import streamlit as st
-from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 # set up page name, icon, layout and sidebar behaviour
